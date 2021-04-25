f33d1a05e6ce55d23200ea1bce10c1839802627d
Phạm Trung Minh <78266272+PhamTrungMinh@users.noreply.github.com>
2021-04-25 12:08:32 +0700

    Update SDL_utils.h

--- SDL_utils.h
+++ SDL_utils.h
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_mixer.h>
 
 using namespace std;
 
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer, const string WINDOW_TITLE,
-             const int SCREEN_HEIGHT, const int SCREEN_WIDTH);
+             const int SCREEN_HEIGHT, const int SCREEN_WIDTH,
+             Mix_Music *bgm, Mix_Chunk *beep, Mix_Chunk *eat, Mix_Chunk *dead);
 void logSDLError(ostream& os, const string &msg, bool fatal);
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer,
+             Mix_Music *bgm, Mix_Chunk *beep, Mix_Chunk *eat, Mix_Chunk *dead);
 void waitUntilKeyPressed();
-
